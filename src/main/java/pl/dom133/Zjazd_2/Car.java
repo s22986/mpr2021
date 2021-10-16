e5ad10426ac673677d3b1bbdb813894b12c289d9
s22986 <s22986@pjwstk.edu.pl>
2021-10-16 11:35:35 +0200

    CarService: Added simple restcontroler

--- src/main/java/pl/dom133/Zjazd_2/Car.java
+++ src/main/java/pl/dom133/Zjazd_2/Car.java
@@ -1,2 +1,91 @@
-package pl.dom133.Zjazd_2;public class Car {
+package pl.dom133.Zjazd_2;
+
+
+public class Car {
+    private int seat;
+    private int yearOfProduction;
+    private int wheels;
+    private int accessoryType;
+    private String model;
+    private String color;
+    private String mark;
+
+    public Car(int seat, int yearOfProduction, int wheels, int accessoryType, String model, String color, String mark) {
+        this.seat = seat;
+        this.yearOfProduction = yearOfProduction;
+        this.wheels = wheels;
+        this.accessoryType = accessoryType;
+        this.model = model;
+        this.color = color;
+        this.mark = mark;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public int getAccessoryType() {
+        return accessoryType;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setAccessoryType(int accessoryType) {
+        this.accessoryType = accessoryType;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return mark+"{" +
+                "seat=" + seat +
+                ", yearOfProduction=" + yearOfProduction +
+                ", wheels=" + wheels +
+                ", accessoryType=" + accessoryType +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", mark='" + mark + '\'' +
+                '}';
+    }
 }
--- src/main/java/pl/dom133/Zjazd_2/CarService.java
+++ src/main/java/pl/dom133/Zjazd_2/CarService.java
@@ -1,2 +1,26 @@
-package pl.dom133.Zjazd_2;public class CarService {
+package pl.dom133.Zjazd_2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarService {
+
+    @GetMapping("/car")
+    public Car cars() {
+        return new Car(5, 2021, 4, 1,
+                "Model 3", "black", "Tesla");
+    }
+
+    public void print(Car car) {
+        System.out.println(car);
+    }
+
+    public void addotionalWheel(Car car) {
+        if(car.getWheels() < 5) {
+            car.setWheels(car.getWheels() + 1);
+        }
+    }
+
+    //Co to serwis i restcontroller, poczytac o wstrzykiwaniu zaleznosci.
 }
